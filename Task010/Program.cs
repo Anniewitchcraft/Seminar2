bf3e62ce7640156b0aadebf6c5f9a18d39ce7f77
Annie <sweetmonkey313@outlook.com>
2023-10-07 13:30:55 +1100

    Task010 done

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,39 +1,24 @@
-﻿// Задача 13: Напишите программу, 
-// которая выводит третью цифру заданного числа 
-// или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+﻿// Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
 
 int Prompt(string message)
 {
-    Console.Write(message);
+    System.Console.Write(message);
     string value = Console.ReadLine();
     int result = Convert.ToInt32(value);
     return result;
 }
 
-int GetThirdRank(int number)
+int number = Prompt("Введите трехзначное число > ");
+if (number < 100 || number >= 1000)
 {
-    while (number > 999)
-    {
-        number /= 10;
-    }
-    return number % 10;
+    Console.WriteLine("Вы ввели не трехзначное число, пожалуйста, повторите ввод");
+    return;
 }
-
-bool ValidateNumber(int number)
-{
-    if (number < 100)
-    {
-        Console.WriteLine("Третьей цифры нет");
-        return false;
-    }
-    return true;
-}
-
-int number = Prompt("Введите число > ");
-if (ValidateNumber(number))
-{
-    Console.WriteLine(GetThirdRank(number));
-}
+Console.WriteLine($"Введеное число `{number}`");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра `{secondRank}`");
